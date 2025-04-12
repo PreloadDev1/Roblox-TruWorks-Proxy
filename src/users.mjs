714e52e5b581ef796c3cc9c0f9a92d744acdac3c
PreloadDev1 <162418719+PreloadDev1@users.noreply.github.com>
2025-04-12 09:36:26 +0100

    Update users.mjs

--- src/users.mjs
+++ src/users.mjs
@@ -1,12 +1,13 @@
-import filterJSON, { getMarketInfo } from "./filterjson.mjs"
+import filterJSON, { getMarketInfo } from "./filterjson.mjs";
 
-const Users = {}
+const Users = {};
 
 Users.getStoreAssets = async function (userId, creatorType, creatorId) {
 	return await filterJSON({
 		url: `https://catalog.roblox.com/v1/search/items/details?CreatorTargetId=${userId}&CreatorType=1&Limit=30`,
 		exhaust: true,
 		filter: getMarketInfo(creatorType, creatorId),
-	})
-}
-export default Users
+	});
+};
+
+export default Users;
