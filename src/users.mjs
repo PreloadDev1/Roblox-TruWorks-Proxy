2eefa484a5ba9ffba78799a2d354bd9b384ba622
PreloadDev1 <162418719+PreloadDev1@users.noreply.github.com>
2025-04-12 08:33:33 +0100

    Update users.mjs

--- src/users.mjs
+++ src/users.mjs
@@ -2,14 +2,12 @@ import filterJSON, { getMarketInfo } from "./filterjson.mjs"
 
 const Users = {}
 
-Users.getStoreAssets = async function(userId) {
-    const storeAssets = await filterJSON({
-        url: `https://catalog.roblox.com/v1/search/items/details?CreatorTargetId=${userId}&CreatorType=1&Limit=30`,
-        exhaust: true,
-        filter: getMarketInfo,
-    })
-
-    return storeAssets
+Groups.getStoreAssets = async function (groupId, creatorType, creatorId) {
+	return await filterJSON({
+		url: `https://catalog.roblox.com/v1/search/items/details?CreatorTargetId=${groupId}&CreatorType=2&Limit=30`,
+		exhaust: true,
+		filter: getMarketInfo(creatorType, creatorId),
+	})
 }
 
 export default Users
