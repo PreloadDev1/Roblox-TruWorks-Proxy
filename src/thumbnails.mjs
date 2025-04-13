cb492e9cbfc350e5afa32e1621a7790dd586c758
PreloadDev1 <162418719+PreloadDev1@users.noreply.github.com>
2025-04-13 19:04:11 -0400

    Update thumbnails.mjs

--- src/thumbnails.mjs
+++ src/thumbnails.mjs
@@ -1,5 +1,5 @@
 export async function getThumbnail(assetId, type = "Asset") {
-    const response = await fetch(`https://thumbnails.roblox.com/v1/assets?assetIds=${assetId}&format=Png&isCircular=false&size=150x150`)
-    const data = await response.json()
-    return data?.data?.[0]?.imageUrl || null
+    const res = await fetch(`https://thumbnails.roblox.com/v1/assets?assetIds=${assetId}&format=Png&isCircular=false&size=150x150`);
+    const data = await res.json();
+    return data?.data?.[0]?.imageUrl || null;
 }
