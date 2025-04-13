0e799e7e5a5323e6ed25b4e4f12d3ac11a901b66
PreloadDev1 <162418719+PreloadDev1@users.noreply.github.com>
2025-04-13 19:06:24 -0400

    Update groups.mjs

--- src/groups.mjs
+++ src/groups.mjs
@@ -3,38 +3,31 @@ import { getThumbnail } from "./thumbnails.mjs";
 
 const Groups = {};
 
-Groups.get = async function (userId) {
-	const groups = await filterJSON({
-		url: `https://groups.roblox.com/v1/users/${userId}/groups/roles?includeLocked=false&includeNotificationPreferences=false`,
-		exhaust: false,
-		filter: (row) => {
-			const group = row.group;
-			if (!group || group.owner?.userId !== userId) return null;
-			return {
-				ID: group.id,
-				Name: group.name,
-			};
-		}
-	});
-
-	return groups;
+Groups.get = async function(userId) {
+    return await filterJSON({
+        url: `https://groups.roblox.com/v1/users/${userId}/groups/roles?includeLocked=false`,
+        exhaust: false,
+        filter: (row) => {
+            const group = row.group;
+            if (!group || group.owner?.userId !== userId) return null;
+            return { ID: group.id, Name: group.name };
+        }
+    });
 };
 
-Groups.getStoreAssets = async function (groupId) {
-	return await filterJSON({
-		url: `https://catalog.roblox.com/v1/search/items?CreatorTargetId=${groupId}&CreatorType=2&Limit=30&SortType=3`,
-		exhaust: true,
-		filter: async function (item) {
-			return {
-				ID: item.id,
-				Name: item.name,
-				Price: item.price,
-				CreatorID: groupId,
-				CreatorType: "Groups",
-				Thumbnail: await getThumbnail(item.id, "Asset"),
-			};
-		}
-	});
+Groups.getStoreAssets = async function(groupId) {
+    return await filterJSON({
+        url: `https://catalog.roblox.com/v1/search/items?CreatorTargetId=${groupId}&CreatorType=2&Limit=30&SortType=3`,
+        exhaust: true,
+        filter: async (item) => ({
+            ID: item.id,
+            Name: item.name,
+            Price: item.price,
+            CreatorID: groupId,
+            CreatorType: "Group",
+            Thumbnail: await getThumbnail(item.id, "Asset"),
+        }),
+    });
 };
 
 export default Groups;
