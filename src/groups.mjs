4bd480d2f57d46f528e5d9209d0eef098596e39c
PreloadDev1 <162418719+PreloadDev1@users.noreply.github.com>
2025-04-14 13:24:08 -0400

    Update groups.mjs

--- src/groups.mjs
+++ src/groups.mjs
@@ -4,30 +4,35 @@ import { getThumbnail } from "./thumbnails.mjs";
 const Groups = {};
 
 Groups.get = async function(userId) {
-    return await filterJSON({
-        url: `https://groups.roblox.com/v1/users/${userId}/groups/roles?includeLocked=false`,
-        exhaust: false,
-        filter: (row) => {
-            const group = row.group;
-            if (!group || group.owner?.userId !== userId) return null;
-            return { ID: group.id, Name: group.name };
-        }
-    });
+	const groups = await filterJSON({
+		url: `https://groups.roblox.com/v1/users/${userId}/groups/roles?includeLocked=false`,
+		exhaust: false,
+		filter: (row) => {
+			const group = row.group;
+			if (!group || group.owner?.userId !== userId) return null;
+			return {
+				ID: group.id,
+				Name: group.name,
+			};
+		}
+	});
+
+	return groups;
 };
 
 Groups.getStoreAssets = async function(groupId) {
-    return await filterJSON({
-        url: `https://catalog.roblox.com/v1/search/items?CreatorTargetId=${groupId}&CreatorType=2&Limit=30&SortType=3`,
-        exhaust: true,
-        filter: async (item) => ({
-            ID: item.id,
-            Name: item.name,
-            Price: item.price,
-            CreatorID: groupId,
-            CreatorType: "Group",
-            Thumbnail: await getThumbnail(item.id, "Asset"),
-        }),
-    });
+	return await filterJSON({
+		url: `https://catalog.roblox.com/v1/search/items?CreatorTargetId=${groupId}&CreatorType=2&Limit=30&SortType=3`,
+		exhaust: true,
+		filter: async (item) => ({
+			ID: item.id,
+			Name: item.name,
+			Price: item.price,
+			CreatorID: groupId,
+			CreatorType: "Group",
+			Thumbnail: await getThumbnail(item.id, "Asset"),
+		})
+	});
 };
 
 export default Groups;
