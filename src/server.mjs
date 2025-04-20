d714a1e9d8489665838841afeecf89b209c129f4
PreloadDev1 <162418719+PreloadDev1@users.noreply.github.com>
2025-04-20 21:24:25 +0100

    Update server.mjs

--- src/server.mjs
+++ src/server.mjs
@@ -2,14 +2,11 @@ import express from "express";
 import cors from "cors";
 
 // Route Imports
-import appRoutes from "./routes/app.mjs";
 import avatarRoutes from "./routes/avatar.mjs";
 import gamesRoutes from "./routes/games.mjs";
 import groupsRoutes from "./routes/groups.mjs";
 import profileRoutes from "./routes/profile.mjs";
 import usersRoutes from "./routes/users.mjs";
-import thumbnailsRoutes from "./routes/thumbnails.mjs";
-
 import devProductsRoutes from "./routes/devproducts.mjs";
 import followersRoutes from "./routes/followers.mjs";
 import friendsRoutes from "./routes/friends.mjs";
@@ -22,20 +19,18 @@ const port = process.env.PORT || 3000;
 app.use(cors());
 
 // Register routes
-app.use("/", appRoutes);
 app.use("/avatar", avatarRoutes);
 app.use("/games", gamesRoutes);
 app.use("/groups", groupsRoutes);
 app.use("/profile", profileRoutes);
 app.use("/users", usersRoutes);
-app.use("/thumbnails", thumbnailsRoutes);
-app.use("/assets", appRoutes); // main.mjs = appRoutes
 app.use("/devproducts", devProductsRoutes);
 app.use("/followers", followersRoutes);
 app.use("/friends", friendsRoutes);
 app.use("/badges", badgesRoutes);
 app.use("/profile/:userId/socials", socialsRoutes);
 
+// Start server
 app.listen(port, () => {
-	console.log(`[TruProxy] Proxy server is live on port ${port}`);
+  console.log(`[TruProxy] Proxy server is live on port ${port}`);
 });
