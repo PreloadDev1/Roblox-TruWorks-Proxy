2e67b4c4b8fd6de671db6bcc0c3898a2876bbe88
PreloadDev1 <162418719+PreloadDev1@users.noreply.github.com>
2025-04-21 13:03:52 +0100

    Update FilterJson.mjs

--- Source/Utilities/FilterJson.mjs
+++ Source/Utilities/FilterJson.mjs
@@ -1,28 +1,28 @@
 export default async function FilterJSON({ URL, Filter, Exhaust }) {
-	const Results = [];
-	let Cursor = "";
-	let Done = false;
+	const Results = []
+	let Cursor = ""
+	let Done = false
 
 	while (!Done) {
-		const Response = await fetch(`${URL}&cursor=${Cursor}`);
-		if (!Response.ok) break;
+		const Response = await fetch(`${URL}&cursor=${Cursor}`)
+		if (!Response.ok) break
 
-		const Body = await Response.json();
-		if (!Array.isArray(Body.data)) break;
+		const Body = await Response.json()
+		if (!Array.isArray(Body.data)) break
 
 		for (const Row of Body.data) {
-			const Result = await Filter(Row);
-			if (Result) Results.push(Result);
+			const Result = await Filter(Row)
+			if (Result) Results.push(Result)
 		}
 
 		if (Exhaust && Body.nextPageCursor) {
-			Cursor = Body.nextPageCursor;
+			Cursor = Body.nextPageCursor
 		} else {
-			Done = true;
+			Done = true
 		}
 	}
 
-	return Results;
+	return Results
 }
 
 export function GetMarketInfo(CreatorType, CreatorID) {
@@ -34,8 +34,8 @@ export function GetMarketInfo(CreatorType, CreatorID) {
 			CreatorType,
 			CreatorID,
 			Thumbnail: Item.thumbnail?.imageUrl || null
-		};
-	};
+		}
+	}
 }
 
 export function GetIdentificationInfo(Item) {
@@ -43,5 +43,5 @@ export function GetIdentificationInfo(Item) {
 		ID: Item.id,
 		Name: Item.name,
 		Thumbnail: Item.thumbnail?.imageUrl || null
-	};
+	}
 }
