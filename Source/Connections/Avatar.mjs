6347ded3d629e177f03aa047b94c453d65f934a0
PreloadDev1 <162418719+PreloadDev1@users.noreply.github.com>
2025-04-21 17:29:36 +0100

    Update Avatar.mjs

--- Source/Connections/Avatar.mjs
+++ Source/Connections/Avatar.mjs
@@ -4,16 +4,22 @@ import GetAvatarAssets from "../Services/AvatarService.mjs"
 const Router = Express.Router()
 
 Router.get("/:UserID", async (Request, Response) => {
-	try {
-		const UserID = parseInt(Request.params.UserID)
-		if (isNaN(UserID)) return Response.status(400).json({ Error: "Invalid User ID" })
+    const UserID = parseInt(Request.params.UserID)
 
-		const Assets = await GetAvatarAssets(UserID)
-		Response.json(Assets)
-	} catch (Error) {
-		console.error("[/avatar/:UserID]", Error)
-		Response.status(500).json({ Error: "Failed to fetch avatar assets" })
-	}
+    if (isNaN(UserID)) {
+        return Response
+            .status(400)
+            .json({ Error: "Invalid User ID" })
+    }
+
+    try {
+        const Assets = await GetAvatarAssets(UserID)
+        Response.json(Assets)
+    } catch {
+        Response
+            .status(500)
+            .json({ Error: "Failed to fetch avatar assets" })
+    }
 })
 
 export default Router
