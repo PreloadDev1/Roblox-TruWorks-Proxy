847bca2194276cd6a0081bf90415bdac86e34505
PreloadDev1 <162418719+PreloadDev1@users.noreply.github.com>
2025-04-21 01:41:10 +0100

    Update GameService.mjs

--- Source/Services/GameService.mjs
+++ Source/Services/GameService.mjs
@@ -1,6 +1,7 @@
 import filterJSON from "../Utilities/FilterJson.mjs";
 import { GetThumbnail } from "./ThumbnailService.mjs";
 import Profile from "./ProfileService.mjs";
+import { ToPascalCaseObject } from "../Utilities/FilterJson.mjs";
 
 const Games = {};
 
