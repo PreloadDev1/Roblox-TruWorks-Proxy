1d4b06e14c0ac726ee569c612de19416efe774e8
PreloadDev1 <162418719+PreloadDev1@users.noreply.github.com>
2025-04-21 18:12:29 +0100

    Update Assets.mjs

--- Source/Connections/Assets.mjs
+++ Source/Connections/Assets.mjs
@@ -3,23 +3,11 @@ import PublicAssetsService from "../Services/PublicAssetsService.mjs"
 
 const Router = Express.Router()
 
-Router.get("/:UserID", async (Request, Response) => {
-    const UserID = parseInt(Request.params.UserID)
-
-    if (isNaN(UserID)) {
-        return Response
-            .status(400)
-            .json({ Error: "Invalid User ID" })
-    }
-
-    try {
-        const Assets = await PublicAssetsService.GetAll(UserID)
-        Response.json(Assets)
-    } catch {
-        Response
-            .status(500)
-            .json({ Error: "Failed to fetch public assets" })
-    }
+Router.get("/:UserID", async (req, res) => {
+  const UserID = parseInt(req.params.UserID)
+  if (isNaN(UserID)) return res.status(400).json({ Error: "Invalid User ID" })
+  const Data = await PublicAssetsService.GetPublicAssets(UserID)
+  res.json(Data)
 })
 
 export default Router
