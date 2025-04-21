9fcf3098de1dd22535503831eaa8ad0e28922beb
PreloadDev1 <162418719+PreloadDev1@users.noreply.github.com>
2025-04-21 02:00:04 +0100

    Update UserService.mjs

--- Source/Services/UserService.mjs
+++ Source/Services/UserService.mjs
@@ -1,14 +1,17 @@
 import FilterJSON from "../Utilities/FilterJson.mjs";
 import { GetMarketInfo } from "../Utilities/FilterJson.mjs";
+import { ToPascalCaseObject } from "../Utilities/ToPascal.mjs";
 
 const Users = {};
 
 Users.GetStoreAssets = async function (UserID, CreatorType, CreatorID) {
-	return await FilterJSON({
+	const Response = await FilterJSON({
 		URL: `https://catalog.roblox.com/v1/search/items/details?CreatorTargetId=${UserID}&CreatorType=1&Limit=30`,
 		Exhaust: true,
 		Filter: GetMarketInfo(CreatorType, CreatorID)
 	});
+
+	return Array.isArray(Response) ? Response.map(ToPascalCaseObject) : [];
 };
 
 export default Users;
