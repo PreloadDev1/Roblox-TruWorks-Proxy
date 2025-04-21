d5c0c4a434707bd6ebf04ea1db4361967fb9153c
PreloadDev1 <162418719+PreloadDev1@users.noreply.github.com>
2025-04-21 13:03:16 +0100

    Update UserService.mjs

--- Source/Services/UserService.mjs
+++ Source/Services/UserService.mjs
@@ -1,34 +1,36 @@
-import FilterJSON from "../Utilities/FilterJson.mjs";
-import { GetMarketInfo } from "../Utilities/FilterJson.mjs";
-import { GetAllPages } from "../Utilities/GetAllPages.mjs";
+import FilterJSON from "../Utilities/FilterJson.mjs"
+import { GetMarketInfo } from "../Utilities/FilterJson.mjs"
+import { GetAllPages } from "../Utilities/GetAllPages.mjs"
 
-const Users = {};
+const Users = {}
 
 Users.GetStoreAssets = async function (UserID, CreatorType, CreatorID) {
-	const URL = `https://catalog.roblox.com/v1/search/items/details?CreatorTargetId=${UserID}&CreatorType=1&Limit=30`;
-	return await GetAllPages(URL, GetMarketInfo(CreatorType, CreatorID));
-};
+	const URL = `https://catalog.roblox.com/v1/search/items/details?CreatorTargetId=${UserID}&CreatorType=1&Limit=30`
+	return await GetAllPages(URL, GetMarketInfo(CreatorType, CreatorID))
+}
 
 Users.GetFollowers = async function (UserID) {
-	const URL = `https://friends.roblox.com/v1/users/${UserID}/followers?limit=100`;
-	const Followers = await GetAllPages(URL, async (Entry) => Entry);
+	const URL = `https://friends.roblox.com/v1/users/${UserID}/followers?limit=100`
+	const Followers = await GetAllPages(URL, async (Entry) => Entry)
+
 	return {
 		Count: Followers.length,
 		List: Followers
-	};
-};
+	}
+}
 
 Users.GetFriends = async function (UserID) {
-	const URL = `https://friends.roblox.com/v1/users/${UserID}/friends?limit=100`;
-	const Friends = await GetAllPages(URL, async (Entry) => Entry);
+	const URL = `https://friends.roblox.com/v1/users/${UserID}/friends?limit=100`
+	const Friends = await GetAllPages(URL, async (Entry) => Entry)
+
 	return {
 		Count: Friends.length,
 		List: Friends
-	};
-};
+	}
+}
 
 Users.GetBadges = async function (UserID) {
-	const URL = `https://badges.roblox.com/v1/users/${UserID}/badges?limit=100`;
+	const URL = `https://badges.roblox.com/v1/users/${UserID}/badges?limit=100`
 
 	const Badges = await GetAllPages(URL, async (Badge) => {
 		return {
@@ -39,13 +41,13 @@ Users.GetBadges = async function (UserID) {
 			AwardedCount: Badge.statistics?.awardedCount || 0,
 			WinRatePercentage: Badge.statistics?.winRatePercentage || null,
 			Thumbnail: Badge.imageUrl || null
-		};
-	});
+		}
+	})
 
 	return {
 		Count: Badges.length,
 		List: Badges
-	};
-};
+	}
+}
 
-export default Users;
+export default Users
