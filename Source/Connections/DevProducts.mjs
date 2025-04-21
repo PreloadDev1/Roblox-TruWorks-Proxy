8ae44e4bac22ad8fb572d1650f9fd4998393046c
PreloadDev1 <162418719+PreloadDev1@users.noreply.github.com>
2025-04-21 17:29:54 +0100

    Update DevProducts.mjs

--- Source/Connections/DevProducts.mjs
+++ Source/Connections/DevProducts.mjs
@@ -1,19 +1,25 @@
 import Express from "express"
-import Profile from "../Services/ProfileService.mjs"
+import ProfileService from "../Services/ProfileService.mjs"
 
 const Router = Express.Router()
 
 Router.get("/:UserID", async (Request, Response) => {
-	try {
-		const UserID = parseInt(Request.params.UserID)
-		if (isNaN(UserID)) return Response.status(400).json({ Error: "Invalid User ID" })
+    const UserID = parseInt(Request.params.UserID)
 
-		const Products = await Profile.GetDevProducts(UserID)
-		Response.json(Products)
-	} catch (Error) {
-		console.error("[/devproducts/:UserID]", Error)
-		Response.status(500).json({ Error: "Failed to fetch developer products" })
-	}
+    if (isNaN(UserID)) {
+        return Response
+            .status(400)
+            .json({ Error: "Invalid User ID" })
+    }
+
+    try {
+        const Products = await ProfileService.GetDevProducts(UserID)
+        Response.json(Products)
+    } catch {
+        Response
+            .status(500)
+            .json({ Error: "Failed to fetch developer products" })
+    }
 })
 
 export default Router
