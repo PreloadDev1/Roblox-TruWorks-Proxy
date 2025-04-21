0217e663ffc5ca3b8ce70f792a472f4ee93e14a4
PreloadDev1 <162418719+PreloadDev1@users.noreply.github.com>
2025-04-21 02:18:59 +0100

    Update Server.mjs

--- Source/Server.mjs
+++ Source/Server.mjs
@@ -2,29 +2,33 @@ import Express from "express";
 import CORS from "cors";
 
 import AvatarRoutes from "./Connections/Avatar.mjs";
-import ProfileRoutes from "./Connections/Profile.mjs";
+import BadgesRoutes from "./Connections/Badges.mjs";
 import DevProductsRoutes from "./Connections/DevProducts.mjs";
 import FollowersRoutes from "./Connections/Followers.mjs";
 import FriendsRoutes from "./Connections/Friends.mjs";
-import BadgesRoutes from "./Connections/Badges.mjs";
-import SocialsRoutes from "./Connections/Socials.mjs";
-import GroupRoutes from "./Connections/Group.mjs";
 import GameRoutes from "./Connections/Game.mjs";
-import AppRoutes from "./Connections/Router.mjs";
+import GroupRoutes from "./Connections/Group.mjs";
+import MainRoutes from "./Connections/Main.mjs";
+import ProfileRoutes from "./Connections/Profile.mjs";
+import RouterRoutes from "./Connections/Router.mjs";
+import SocialsRoutes from "./Connections/Socials.mjs";
 
 const App = Express();
 const Port = process.env.PORT || 3000;
 
 App.use(CORS());
 
+App.use("/avatar", AvatarRoutes);
+App.use("/badges", BadgesRoutes);
 App.use("/devproducts", DevProductsRoutes);
 App.use("/followers", FollowersRoutes);
 App.use("/friends", FriendsRoutes);
-App.use("/badges", BadgesRoutes);
-App.use("/profile/:userId/socials", SocialsRoutes);
-App.use("/group", GroupRoutes);
 App.use("/game", GameRoutes);
-App.use("/", AppRoutes);
+App.use("/group", GroupRoutes);
+App.use("/main", MainRoutes);
+App.use("/profile", ProfileRoutes);
+App.use("/router", RouterRoutes);
+App.use("/socials", SocialsRoutes);
 
 App.listen(Port, () => {
 	console.log(`[TruProxy] Proxy server is live on port ${Port}`);
