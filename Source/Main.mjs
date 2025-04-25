75778c092a0732d8d28561dd260b111f7f63e7e2
PreloadDev1 <162418719+PreloadDev1@users.noreply.github.com>
2025-04-26 00:08:25 +0100

    Update Main.mjs

--- Source/Main.mjs
+++ Source/Main.mjs
@@ -1,39 +1,8 @@
-import Groups from "./Groups.mjs"
-import Games, { CreatorTypes } from "./Games.mjs"
+import Profile from "./Profile.mjs"
 
-
-async function getPublicAssets(userId) {
-    const games = await Games.get(userId, CreatorTypes.User)
-    const groups = await Groups.get(userId)
-
-    let result = {
-        gamePasses: [],
-        groupGamePasses: [],
-        groupStoreAssets: [],
-    }
-    
-    for (const game of games) {
-        const gamePasses = await Games.getPasses(game.id)
-
-        result.gamePasses.push(...gamePasses)
-    }
-
-    for (const group of groups) {
-        const groupGames = await Games.get(group.id, CreatorTypes.Group)
-        const storeAssets = await Groups.getStoreAssets(group.id)
-        
-        for (const game of groupGames) {
-            const gamePasses = await Games.getPasses(game.id)
-
-            result.groupGamePasses.push(...gamePasses)
-        }
-
-        result.groupStoreAssets.push(...storeAssets)
-    }
-
-    return result
+async function Main(UserID) {
+    const Result = await Profile.GetProfileData(UserID)
+    console.log(Result)
 }
 
-
-const result = await getPublicAssets(/*User Id*/)
-console.log(result)
+await Main(/* UserID */)
