d2d8e80db1ea17b52fdeb348fe103fa3ad3c6281
PreloadDev1 <162418719+PreloadDev1@users.noreply.github.com>
2025-04-25 23:59:25 +0100

    Update Followers.mjs

--- Source/Followers.mjs
+++ Source/Followers.mjs
@@ -1,1 +1,36 @@
+import filterJSON from "./FilterJson.mjs"
 
+const Followers = {}
+
+Followers.GetFollowerInfo = async function(UserID) {
+    const FollowersList = await filterJSON({
+        url: `https://friends.roblox.com/v1/users/${UserID}/followers?limit=100`,
+        exhaust: true,
+        filter: user => {
+            return {
+                UserID: user.id,
+                Username: user.name,
+                DisplayName: user.displayName,
+                IsBanned: false,
+                IsVerified: user.hasVerifiedBadge || false,
+                Description: "",
+                Created: {
+                    Year: 0,
+                    Month: 0,
+                    Day: 0,
+                    Hour: 0,
+                    Minute: 0,
+                    Second: 0,
+                    Millisecond: 0,
+                },
+            }
+        },
+    })
+
+    return {
+        FollowerCount: FollowersList.length,
+        Followers: FollowersList,
+    }
+}
+
+export default Followers
