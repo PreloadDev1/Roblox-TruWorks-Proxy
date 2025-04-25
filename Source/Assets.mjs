cefdf70f81eabd02addd7e96a7070504b40a2d33
PreloadDev1 <162418719+PreloadDev1@users.noreply.github.com>
2025-04-25 23:57:55 +0100

    Update Assets.mjs

--- Source/Assets.mjs
+++ Source/Assets.mjs
@@ -1,1 +1,52 @@
+import Groups from "./Groups.mjs"
+import Games, { CreatorTypes } from "./Games.mjs"
+import filterJSON, { getMarketInfo } from "./FilterJson.mjs"
 
+const Assets = {}
+
+Assets.GetPublicAssets = async function(UserID) {
+    const Result = {
+        UserPasses: [],
+        GroupPasses: [],
+        UserMerch: [],
+        GroupMerch: [],
+    }
+
+    const GamesList = await Games.get(UserID, CreatorTypes.User)
+
+    for (const Game of GamesList) {
+        const Passes = await Games.getPasses(Game.id)
+        Result.UserPasses.push(...Passes)
+    }
+
+    const UserItems = await filterJSON({
+        url: `https://catalog.roblox.com/v1/search/items?category=11&creatorType=User&creatorTargetId=${UserID}&limit=30&salesTypeFilter=1&sortOrder=Asc`,
+        exhaust: true,
+        filter: getMarketInfo,
+    })
+
+    Result.UserMerch.push(...UserItems)
+
+    const GroupsList = await Groups.get(UserID)
+
+    for (const Group of GroupsList) {
+        const GroupGames = await Games.get(Group.id, CreatorTypes.Group)
+
+        for (const Game of GroupGames) {
+            const Passes = await Games.getPasses(Game.id)
+            Result.GroupPasses.push(...Passes)
+        }
+
+        const GroupItems = await filterJSON({
+            url: `https://catalog.roblox.com/v1/search/items?category=11&creatorType=Group&creatorTargetId=${Group.id}&limit=30&salesTypeFilter=1&sortOrder=Asc`,
+            exhaust: true,
+            filter: getMarketInfo,
+        })
+
+        Result.GroupMerch.push(...GroupItems)
+    }
+
+    return Result
+}
+
+export default Assets
