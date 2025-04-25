38e010118857ebcb3cbae3205ec20ab5abc388ce
PreloadDev1 <162418719+PreloadDev1@users.noreply.github.com>
2025-04-25 23:58:22 +0100

    Update Badges.mjs

--- Source/Badges.mjs
+++ Source/Badges.mjs
@@ -1,1 +1,36 @@
+import filterJSON from "./FilterJson.mjs"
+import Games from "./Games.mjs"
 
+const Badges = {}
+
+Badges.GetBadgeInfo = async function(UserID) {
+    const BadgesList = await filterJSON({
+        url: `https://badges.roblox.com/v1/users/${UserID}/badges?limit=100&sortOrder=Desc`,
+        exhaust: true,
+        filter: badge => {
+            return {
+                ID: badge.id,
+                Name: badge.name,
+                Description: badge.description,
+                AwardedCount: badge.awardedCount,
+                WinRatePercentage: badge.winRatePercentage,
+                Created: badge.created,
+                Updated: badge.updated,
+                PlaceID: badge.statistics.placeId,
+                UniverseID: badge.statistics.universeId,
+            }
+        },
+    })
+
+    for (const Badge of BadgesList) {
+        const GameInfo = await Games.getByUniverseId(Badge.UniverseID)
+        Badge.GameInfo = GameInfo
+    }
+
+    return {
+        BadgeCount: BadgesList.length,
+        Badges: BadgesList,
+    }
+}
+
+export default Badges
